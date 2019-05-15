5b077a980c336a8a931e53ed4e820a5ad47d8470
safwanmoha <safwan.mohammed@trycake.com>
2019-05-15 10:19:48 +0530

    fixing empty search string issue

--- install.sh
+++ install.sh
@@ -6,18 +6,22 @@ if [ "/usr/local/bin/save" = "$0" ]
 then
 	if [ $1 = "-s" ]
 	then
-		search=""
-		for ((i=2;i<=$#;i++));
-		do
-			search="$search ${!i}"
-		done;
-		sh -c "grep $search /var/lib/save/history -m 10 -n -i"
-		read -p 'copy line: ' linenum
-		result=$(head -${linenum} /var/lib/save/history | tail -1 | tr -d '\012')
-		hay=$(echo $result| cut -d':' -f 2)
-		sh -c "echo $hay | tr -d '\012' | xclip -selection clipboard"
-		echo "$(tput setaf 4)Copied...$(tput sgr0) $hay"
-
+		if [ "$2" = "" ]
+		then
+			echo "Wrong usage... $(tput setaf 2)to search: save -s 'search-text'$(tput sgr0)"
+		else
+			search=""
+			for ((i=2;i<=$#;i++));
+			do
+				search="$search ${!i}"
+			done;
+			sh -c "grep $search /var/lib/save/history -m 10 -n -i"
+			read -p 'copy line: ' linenum
+			result=$(head -${linenum} /var/lib/save/history | tail -1 | tr -d '\012')
+			hay=$(echo $result| cut -d':' -f 2)
+			sh -c "echo $hay | tr -d '\012' | xclip -selection clipboard"
+			echo "$(tput setaf 4)Copied...$(tput sgr0) $hay"
+		fi
 	elif [ $1 = "-h" ]
 	then
 		echo "$(tput setaf 4)to save: save 'any-command'$(tput sgr0)"
