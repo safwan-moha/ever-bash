ce037a29c9f329294649616e7f4be7ccd49cf4d8
safwanmoha <safwan.mohammed@trycake.com>
2019-05-25 12:11:42 +0530

    implementing save command to save last command

--- install.sh
+++ install.sh
@@ -15,9 +15,9 @@ then
 			do
 				search="$search ${!i}"
 			done;
-			sh -c "grep $search /var/lib/save/history -m 20 -n -i | sed 's/:/  :/'"
+			sh -c "grep $search ~/.save/history -m 20 -n -i | sed 's/:/  :/'"
 			read -p 'copy line: ' linenum
-			result=$(head -${linenum} /var/lib/save/history | tail -1 | tr -d '\012')
+			result=$(head -${linenum} ~/.save/history | tail -1 | tr -d '\012')
 			hay=$(echo $result| cut -d'#' -f 2)
 			sh -c "echo $hay | tr -d '\012' | xclip -selection clipboard"
 			echo "$(tput setaf 4)Copied:$(tput sgr0) $(tput setaf 1)$hay$(tput sgr0)"
@@ -31,14 +31,24 @@ then
 		echo "$(tput setaf 4)to search: save -s 'search-text'$(tput sgr0)"
 		echo "$(tput setaf 4)to delete: save -d 'search-text'$(tput sgr0)"
 	else
-		command=""
-		for ((i=1;i<=$#;i++));
-		do
-			command="$command ${!i}"
-		done;
-		read -p 'save-name: ' savename
-		sudo sh -c "echo $savename#$command >> /var/lib/save/history"
-		echo "$(tput setaf 4)Saved...$(tput sgr0)"
+		if [ "$2" = "" ]
+		then
+			echo "$(tput setaf 1)Wrong usage:$(tput sgr0) $(tput setaf 2)to search: save -s 'search-text'$(tput sgr0)"
+			command=$(history | tail -n 2 | head -n 1 | while read c1 c2; do echo $c2; done)
+			echo "$(tput setaf 4)Last command: $(tput sgr0) $command"
+			read -p 'save-name: ' savename
+			sudo sh -c "echo $savename#$command >> ~/.save/history"
+			echo "$(tput setaf 4)Saved...$(tput sgr0)"
+		else
+			command=""
+			for ((i=1;i<=$#;i++));
+			do
+				command="$command ${!i}"
+			done;
+			read -p 'save-name: ' savename
+			sudo sh -c "echo $savename#$command >> ~/.save/history"
+			echo "$(tput setaf 4)Saved...$(tput sgr0)"
+		fi
 	fi
 else
 	echo " "
@@ -50,10 +60,10 @@ else
 	sh -c "sudo cp $MY_PATH/install.sh /usr/local/bin/save"
 	sh -c "sudo chmod -R 775 /usr/local/bin/save"
 	if [ ! -d /var/lib/save ]; then
-		sh -c "sudo mkdir /var/lib/save"
+		sh -c "mkdir ~/.save"
 	fi
-	sh -c "sudo touch /var/lib/save/history"
-	sh -c "sudo chmod -R 775 /var/lib/save/history"
+	sh -c "touch ~/.save/history"
+	# sh -c "sudo chmod -R 775 /var/lib/save/history"
 	echo "$(tput setaf 2)Installation success$(tput sgr0)"
 	echo "$(tput setaf 4)Please type 'save -h' for help$(tput sgr0)"
 fi
